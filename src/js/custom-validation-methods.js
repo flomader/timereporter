a3833ed8f0a30230fd97cbdd6877937dccb1f9ac
Florian Mader <florian.mader@outlook.com>
2020-07-11 23:09:19 +0200

    added settings

---
+++ src/js/custom-validation-methods.js
@@ -0,0 +1,21 @@
+$.validator.addMethod( "laterThan", function( value, element, param ) {
+    var target = $( param );
+
+    return ((new Date('01/01/1970 ' + value)) > (new Date('01/01/1970 ' + target.val())));
+}, "Please enter a later time." );
+
+$.validator.addMethod( "time", function( value, element ) {
+	return this.optional( element ) || /^([01]\d|2[0-3]|[0-9])(:[0-5]\d){1,2}$/.test( value );
+}, "Please enter a valid time, between 00:00 and 23:59" );
+
+$.validator.addMethod( "greaterThan", function( value, element, param ) {
+    var target = $( param );
+
+    if ( this.settings.onfocusout && target.not( ".validate-greaterThan-blur" ).length ) {
+        target.addClass( "validate-greaterThan-blur" ).on( "blur.validate-greaterThan", function() {
+            $( element ).valid();
+        } );
+    }
+
+    return parseInt(value) > parseInt(target.val());
+}, "Please enter a greater value." );
---
+++ src/js/options.js
@@ -0,0 +1,138 @@
+$(document).ready(function(){
+    $('#settingsForm').validate({
+        onfocusout: false,
+        onkeyup: false,
+        onclick: false,
+        rules: {
+            minhours: {
+                required: true,
+                digits: true,
+                min: 0,
+                max: 11
+            },
+            maxhours: {
+                required: true,
+                digits: true,
+                min: 1,
+                max: 12,
+                greaterThan: '#minhours'
+            },
+            earliestbegin: {
+                required: true,
+                time: true
+            },
+            latestbegin: {
+                required: true,
+                time: true,
+                laterThan: '#earliestbegin'
+            }
+        },
+        messages: {
+            minhours: {
+                required: '"Min. Hours/Day" is a required field.',
+                digits: '"Min. Hours/Day" accepts only digits.',
+                min: jQuery.validator.format('"Min. Hours/Day" must be greater than {0}.'),
+                max: jQuery.validator.format('"Min. Hours/Day" must be less or equal than {0}.')
+            },
+            maxhours: {
+                required: '"Max. Hours/Day" is a required field.',
+                digits: '"Max. Hours/Day" accepts only digits.',
+                min: jQuery.validator.format('"Max. Hours/Day" must be greater than {0}.'),
+                max: jQuery.validator.format('"Max. Hours/Day" must be less or equal than {0}.'),
+                greaterThan: '"Max. Hours/Day" must be greater than "Min. Hours/Day".'
+            },
+            earliestbegin: {
+                required: '"Earliest Begin" is a required field.',
+                time: '"Earliest Begin" must be a valid time, between 00:00 and 23:59.'
+            },
+            latestbegin: {
+                required: '"Latest Begin" is a required field.',
+                time: '"Latest Begin" must be a valid time, between 00:00 and 23:59.',
+                laterThan: '"Latest Begin" must be later than "Earliest Begin".'
+            }
+        },
+        errorElement: 'div',
+        errorClass: 'messagetext',
+        errorPlacement: function(error, element) {
+            error.wrap('<div class="message"></div>');
+            $('<i class="ms-Icon ms-Icon--ErrorBadge erroricon"></i>').insertBefore(error);
+            error.parent().appendTo($('#messagebar'));
+        }
+    });
+
+    chrome.storage.sync.get('settings', function(response){
+        if (typeof response.settings !== "undefined" && response.settings.minHours){
+            $('#minhours').val(response.settings.minHours);
+        }
+        else
+        {
+            $('#minhours').val(defaultMinHours);
+        }
+
+        if (typeof response.settings !== "undefined" && response.settings.maxHours){
+            $('#maxhours').val(response.settings.maxHours);
+        }
+        else
+        {
+            $('#maxhours').val(defaultMaxHours);
+        }
+
+        if (typeof response.settings !== "undefined" && response.settings.earliestBegin){
+            $('#earliestbegin').val(response.settings.earliestBegin);
+        }
+        else
+        {
+            $('#earliestbegin').val(defaultEarliestBegin);
+        }
+
+        if (typeof response.settings !== "undefined" && response.settings.latestBegin){
+            $('#latestbegin').val(response.settings.latestBegin);
+        }
+        else
+        {
+            $('#latestbegin').val(defaultLatestBegin);
+        }
+    });
+
+    $('#save').click(function(){
+
+        $("#messagebar").hide();
+        $(".message").remove();
+
+        if ($('#settingsForm').valid()){
+
+            var minHours = $('#minhours').val();
+            var maxHours = $('#maxhours').val();
+            var earliestBegin = $('#earliestbegin').val();
+            var latestBegin = $('#latestbegin').val();
+            
+            chrome.storage.sync.set({'settings': {
+                                                    'minHours': minHours,
+                                                    'maxHours': maxHours, 
+                                                    'earliestBegin': earliestBegin,
+                                                    'latestBegin': latestBegin
+                                                }}, function(){
+                console.log('Settings have been saved.');
+            });
+            $('#messagebar').attr('hasErrors', 'false');
+            $('#messagebar').append('<div class="message"><i class="ms-Icon ms-Icon--Completed successicon"></i><div class="messagetext">Settings have been saved successfully.</div></div>');
+
+        }
+        else
+        {
+            $('#messagebar').attr('hasErrors', 'true');
+        }
+
+        $('#messagebar').show();
+    });
+
+    $('#reset').click(function(){
+        $("#messagebar").hide();
+        $(".error").remove();
+
+        $('#minhours').val(defaultMinHours);
+        $('#maxhours').val(defaultMaxHours);
+        $('#earliestbegin').val(defaultEarliestBegin);
+        $('#latestbegin').val(defaultLatestBegin);
+    })
+});
