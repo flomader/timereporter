f5b0b5fc84489bce05f27ec2389d90e5f87ae41b
Florian Mader <florian.mader@outlook.com>
2020-07-13 14:17:51 +0200

    bugfix only fill out times into workdays (not weekend or public holiday)

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://microsoftedge.microsoft.com/addons/detail/hmkcjkkdjhmobmgafmjgkaelndlopb
 The extension is hidden in the store, thus only available through this link and not via Microsoft Store Search.
 
 ## Usage
-Navigate to MSVacation WTT in Microsoft Edge (Chromium). Click the time reporter icon next to your browser's address bar. The extension generates new times and inserts them into the required fields each time you click the icon.
+Navigate to MSVacation WTT in Microsoft Edge (Chromium). Click the time reporter icon next to your browser's address bar. The extension generates new times and inserts them into the required fields each time you click the icon. The extension only populates workdays. It does not insert times on weekends or public holidays.
 
 ## Settings
 To enter the extension's settings, click on the Time Reporter icon and select "Extension Options".  
--- src/js/content.js
+++ src/js/content.js
@@ -1,15 +1,27 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.command == "populateWorkingTime"){
-        $("[id^=ctl00_ContentPlaceHolder1_Change_183_]").each(function(i){
-            $(this).attr('value', 1);
+    if(request.command == 'populateWorkingTime'){
+        $('[id^=ctl00_ContentPlaceHolder1_Change_183_]').each(function(i){
+            if (isWorkday($(this))){
+                $(this).attr('value', 1);
+            }
         });
 
-        $("[id^=ctl00_ContentPlaceHolder1_StartTime_183_]").each(function(i){
-            $(this).attr('value', request.data[i].startTime);
+        $('[id^=ctl00_ContentPlaceHolder1_StartTime_183_]').each(function(i){
+            if (isWorkday($(this))){
+                $(this).attr('value', request.data[i].startTime);
+            }
         });
 
-        $("[id^=ctl00_ContentPlaceHolder1_EndTime_183_]").each(function(i){
-            $(this).attr('value', request.data[i].endTime);
-        })
+        $('[id^=ctl00_ContentPlaceHolder1_EndTime_183_]').each(function(i){
+            if (isWorkday($(this))){
+                $(this).attr('value', request.data[i].endTime);
+            }
+        });
     }
-})
+});
+
+function isWorkday(elem){
+    if (elem.closest('tr').css('background-color') == 'rgb(255, 255, 255)') return true;
+    
+    return false;
+}
